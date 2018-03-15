78f4c9ed9a98441003af1bd64fd856499771c250
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-15 09:40:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  nl = document.querySelectorAll('.ranked-list li')  
+  for (i=0; i > nl.length; i++){
+    
+  }
+}
+
