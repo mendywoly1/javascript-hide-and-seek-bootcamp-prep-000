5425f94e03bf21669788975c46cc8c7a6fd4bcd1
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-15 10:49:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+
+function deepestChild(){
+  var deepchild = document.querySelectorAll('#grand-node div')
+  return deepchild[deepchild.length - 1]
+}
+
+function increaseRankBy(n){
+  // node of all class .ranked-list
+  var arl = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < arl[0].children.length; i++){
+    arl[0].children[i].innerHTML = parseInt(arl[0].children[i].innerHTML) + n
+  }
+  
+    for (let i = 0; i < arl[1].children.length; i++){
+      arl[1].children[i].innerHTML = parseInt(arl[1].children[i].innerHTML) + n
+    }
+
+}
+
