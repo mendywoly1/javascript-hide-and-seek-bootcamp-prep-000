d1439846c3377be2f9ca8a927317effdaecd6976
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-15 10:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function increaseRankBy(n){
   // node of all class .ranked-list
   var arl = document.querySelectorAll('.ranked-list')
   // make a variable for both lists
-  firstList = arl[0]
-  secondList = arl[1]
+ var firstList = arl[0]
+  var secondList = arl[1]
   // to get to list go to firstList.children[].innerHTML
   let start = 1
   for (let i = 0, l = children.length; i < l; i++){
